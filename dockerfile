0e6d15a8fbf1880845223f2ec3f53b697c5674a8
aadeshkale <aadeshkale619@gmail.com>
2024-01-06 22:43:39 +0530

    main code added

---
+++ dockerfile
@@ -0,0 +1,2 @@
+FROM python:3.11.7-alpine
+CMD ["python3","main.py"]
--- main.py
+++ main.py
@@ -5,16 +5,22 @@ import yaml
 
 file_path = os.getenv("file_path")
 file_name = os.getenv("file_name")
-yaml_key = os.getenv("yaml_key") # spec.template.spec.image
+yaml_key = os.getenv("yaml_key") # spec.template.spec.image.containers.0.image
 yaml_value = os.getenv("yaml_value")
 
-with open(f"{file_path}/{file_name}",'r+') as file:
-    contents = yaml.safe_load(file)
-    print(contents["spec"]["template"])
-    if '.' in yaml_key:
-        key_path = []
-        rst = yaml_key.split('.')
-        print(rst)
+yaml_file = f"{file_path}/{file_name}"
+data = yaml.safe_load(open(yaml_file, "rb"))
+
+def get_updated_dict(dict_to_update, path, value):
+    obj = dict_to_update
+    key_list = path.split(".")
+    for k in key_list[:-1]:
+        if k in '0123456789':
+            k = int(k)
+        obj = obj[k]
+    obj[key_list[-1]] = value
 
 if __name__ == "__main__":
-    print("Processing...")
+    get_updated_dict(data,yaml_key,yaml_value)
+    yaml.dump(data, open(yaml_file, "w"), default_flow_style=False)
+    
