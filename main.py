062a73cbaedeae62ed623456aabb77173ac588e1
aadeshkale <aadeshkale619@gmail.com>
2024-01-07 14:05:28 +0530

    origin corrected

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
     yaml.dump(data, open(yaml_file, "w"), default_flow_style=False)
     cloned_repo.git.add('--all')
     cloned_repo.git.commit('-m', f'edzyaml has updated {YAML_VALUE} in yaml', author=f'{GIT_USERNAME}')
-    origin = cloned_repo.remote(name='main')
+    origin = cloned_repo.remote(name='origin')
     origin.push()
